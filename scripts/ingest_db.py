da3ad68a6fbdced5f7c436766681d51ede073c68
MoamenMetwally <141279506+MoamenMetwally@users.noreply.github.com>
2024-04-16 00:27:35 +0200

    Update ingest_db.py

--- scripts/ingest_db.py
+++ scripts/ingest_db.py
@@ -16,20 +16,21 @@ mysql_connect.commit()
 # Open JSON files
 apps_catalogue_path='/user-docs/apps_json'
 for file in os.listdir(apps_catalogue_path):
-    with open(os.path.join(apps_catalogue_path, file), 'r') as f:
-        data = json.load(f)
+    if file.endswith('.json'):
+        with open(os.path.join(apps_catalogue_path, file), 'r') as f:
+            data = json.load(f)
 
-    # Insert each data into the MySQ table
-    system_name = data['system_name']
-    system_build_ver = data['system_build_ver']
-    app_name = data['app_name']
-    app_version = data['app_version']
-    app_compiler_ver = data['app_compiler_ver']
-    classification = data['app_classification']
-    description = data['description']
+        # Insert each data into the MySQ table
+        system_name = data['system_name']
+        system_build_ver = data['system_build_ver']
+        app_name = data['app_name']
+        app_version = data['app_version']
+        app_compiler_ver = data['app_compiler_ver']
+        classification = data['app_classification']
+        description = data['description']
 
-    record = "INSERT INTO apps (system_name,system_build_ver,app_name,app_ver,compiler_ver,classification,description) VALUES (%s,%s,%s,%s,%s,%s,%s)"
-    cursor.execute(record, (system_name,system_build_ver,app_name,app_version,app_compiler_ver,classification,description))
+        record = "INSERT INTO apps (system_name,system_build_ver,app_name,app_ver,compiler_ver,classification,description) VALUES (%s,%s,%s,%s,%s,%s,%s)"
+        cursor.execute(record, (system_name,system_build_ver,app_name,app_version,app_compiler_ver,classification,description))
 
 update_query = "UPDATE apps t1 JOIN pre_class t2 ON t1.app_name = t2.app_name SET t1.classification = t2.classification"
 cursor.execute(update_query)
